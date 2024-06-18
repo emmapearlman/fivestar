8078cc64eee440b2cbf9e985d2f6a6812dcce764
Emma Pearlman <emma.pearlman@gmail.com>
2024-06-18 21:13:52 +0100

    update readme

--- src/App.js
+++ src/App.js
@@ -31,9 +31,9 @@ function App() {
   let filledStar = '★';
 
   const handleClick = (starId) => {
-    const arrayLength = InitialStars.length;
+    const arrayLength = InitialStars.length; // array is five in this example, but could be any length
     let newStars = InitialStars.filter(star => star.id <= starId);
-    console.log(newStars);
+    console.log(newStars); //log filtered array
     let nextStars = newStars.map(next => {
 
       return {
@@ -41,13 +41,14 @@ function App() {
         star: filledStar,
       };
     });
-
+    // log array with filled stars
     console.log(nextStars);
+    // add array elements up to length
     let list = nextStars;
     for (let i = starId + 1; i <= arrayLength; i++) {
       list = list.concat({ id: i, star: '☆' });
     }
-
+    // log new array
     console.log(list)
     setInitialStars(list);
 
